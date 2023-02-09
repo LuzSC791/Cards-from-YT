65394701719886e81a0c277cd50539758f3faf77
LuzSC791 <luzsuarez791@gmail.com>
2023-02-08 22:45:15 -0600

    tarjetaBotones

---
+++ main.js
@@ -0,0 +1,7 @@
+function openWindow() {
+    let nav = document.getElementById("navegation-window");
+    if (nav.style.display == "none") 
+        nav.style.display = "block";
+    else 
+        nav.style.display = "none";
+}
